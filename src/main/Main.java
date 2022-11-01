a353830cf02a4f60e7532ab3512a296b16a2251c
João Pedro Juvino dos Santos <joao.pedro.santos@ccc.ufcg.edu.br>
2022-10-31 21:06:49 -0300

    First commit

---
+++ src/main/Main.java
@@ -0,0 +1,18 @@
+package main;
+
+public class Main {
+	public static void main(String[] args) {
+		int[] array = {1,2,3,4,5,6,7,8,9};
+		
+		if (buscaBinaria(4, array, 0, array.length))
+			System.out.println("Está presente!");
+		else
+			System.out.println("Não está presente!");
+		
+	}
+	
+	public static boolean buscaBinaria(int valor, int[] array, int leftIndex, int rightIndex) {
+		return true;
+	}
+
+}
